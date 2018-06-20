2b6a81f00f20da8cdb785b38d08f7133347fa29e
Anish Malhotra <technophile.m7@gmail.com>
2018-06-20 20:52:34 +0530

    sol 2

---
+++ Assignment3/2.java
@@ -0,0 +1,24 @@
+package q2;
+import java.util.Scanner;
+
+public class ques2 {
+	 public static void main(String[] args) {
+		 Scanner key=new Scanner(System.in);
+		 String name;
+		 int age;
+		 int salary;
+		 System.out.println("enter your name");
+		 name=key.next();
+		 System.out.println(name);
+		 System.out.println("enter your age");
+		 age=key.nextInt();
+		 System.out.println(age);
+		 System.out.println("salary");
+		 salary=key.nextInt();
+		 System.out.println(salary);
+
+
+
+		 
+	 }
+}
