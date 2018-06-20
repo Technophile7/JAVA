c660ba7236001b5d6b700af23bca35f44d78aa52
Anish Malhotra <technophile.m7@gmail.com>
2018-06-20 20:59:10 +0530

    sol 5

---
+++ Assignment3/5.java
@@ -0,0 +1,22 @@
+package q5;
+
+import java.util.Scanner;
+
+public class ques5 {
+
+		 public static void main(String[] args) {
+			 Scanner key=new Scanner(System.in);
+			 String name;
+			 float a;
+			 float b,c,d;
+			 System.out.println("enter three no.s");
+			a=key.nextFloat();
+			b=key.nextFloat();
+			c=key.nextFloat();
+            d=(a+b+c)/2;
+            System.out.println(d);
+
+
+			 
+		 }
+	}
