6fce896f405bff30d76508c6d1cd8ba2168572c3
Anish Malhotra <technophile.m7@gmail.com>
2018-06-20 20:58:47 +0530

    sol 4

---
+++ Assignment3/4.java
@@ -0,0 +1,26 @@
+package q4;
+
+import java.util.Scanner;
+
+public class ques4 {
+
+		 public static void main(String[] args) {
+			 Scanner key=new Scanner(System.in);
+			 String name;
+			 int age;
+			 System.out.println("enter your name");
+			 name=key.next();
+			 System.out.println(name);
+			 System.out.println("enter your age");
+			 age=key.nextInt();
+			 System.out.println(age);
+			 System.out.println("your age frm 5 yrs nw");
+			 System.out.println(age+5);
+			 System.out.println("your age 5 yrs ago frm  nw");
+
+			 System.out.println(age-5);
+
+
+			 
+		 }
+	} 
