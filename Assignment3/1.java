da3448efc6ab320b83394112e61b1ad11ad6a649
Anish Malhotra <technophile.m7@gmail.com>
2018-06-20 20:52:03 +0530

    sol 1

---
+++ Assignment3/1.java
@@ -0,0 +1,24 @@
+package q1;
+
+import java.util.Scanner;
+
+public class ques1 {
+
+	 
+	 public static void main(String[] args) {
+	        Scanner keyboard = new Scanner(System.in);
+	        System.out.println("input a word");
+	        keyboard.next();
+	        System.out.println("input second word ");
+	        keyboard.next();
+	        System.out.println();
+	        System.out.println("input a number");
+	        keyboard.nextInt();
+	        System.out.println("input second number ");
+	        keyboard.nextInt();
+	        System.out.println();
+
+
+	}
+
+}
