23acb04d7a8164e802c39200ca8214bb99b31a44
Anish Malhotra <technophile.m7@gmail.com>
2018-06-21 21:26:01 +0530

    sol 20

---
+++ Assignment3/20.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class ques20 {
+
+	 public static void main(String[] args) {
+
+		 Scanner key=new Scanner(System.in);
+         System.out.println("enter any no negative no.");
+		 int a=key.nextInt();
+	 	for(double i=a;i<=10;)
+	 		{System.out.println(i);
+             i=i+0.5;
+	 		}
+
+	   
+	 }
+	}
