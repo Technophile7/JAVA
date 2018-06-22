0ac7f9682ebb127583f21668a637dd7929a6309e
Anish Malhotra <technophile.m7@gmail.com>
2018-06-22 20:02:33 +0530

    sol 1

---
+++ Assignment4/2.java
@@ -0,0 +1,21 @@
+package avg3;
+
+import java.util.Scanner;
+
+public class avg {
+
+	 public static void main(String[] args) {
+
+		 Scanner key=new Scanner(System.in);
+		 System.out.println("enter 3 no.s");
+		 
+		    Float a=key.nextFloat();
+		    Float b=key.nextFloat();
+		    Float c=key.nextFloat();
+	        Float d;
+	        d=(a+b+c)/3;
+	   	 System.out.println("avg is"+a);
+	    	
+		
+	}
+	}
