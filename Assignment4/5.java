ecaac3b6f2f822b6b2b67b5a662cdadce6f916a7
Anish Malhotra <technophile.m7@gmail.com>
2018-06-22 20:04:05 +0530

    sol 5

---
+++ Assignment4/5.java
@@ -0,0 +1,23 @@
+package pentagonal;
+
+
+
+public class penta {
+
+
+	 public static void main(String[] args) {
+
+		
+		 System.out.println("first 50 pentagonal no.s are");
+		  
+       
+	  
+	   	for(int i=1;i<51;i++)
+	   	{
+       int s=i*((3*i)-1);
+       int y=s/2;
+	   		System.out.println(y);
+	   	}
+	   	
+	   	}
+	}
