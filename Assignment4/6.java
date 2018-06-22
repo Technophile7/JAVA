9930d5908320d67cb1343e13d1385130ce5fb8a1
Anish Malhotra <technophile.m7@gmail.com>
2018-06-22 20:04:22 +0530

    sol 6

---
+++ Assignment4/6.java
@@ -0,0 +1,23 @@
+package leap;
+
+import java.util.Scanner;
+
+public class year {
+
+
+	 public static void main(String[] args) {
+
+		 Scanner key=new Scanner(System.in);
+		int n ; 
+		 System.out.println("enter the year");
+		  n=key.nextInt();
+       
+	   	if(n%4==0)
+	   	{
+	   	
+	   	
+	   		System.out.println("this is a leap year");
+	   	}
+	
+	 }}
+
