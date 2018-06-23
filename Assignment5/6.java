d30aaf8bc814ea0761cd857a406df390473318ab
Anish Malhotra <technophile.m7@gmail.com>
2018-06-23 22:53:15 +0530

    sol 6

---
+++ Assignment5/6.java
@@ -0,0 +1,18 @@
+package as5Q6;
+
+import java.util.Scanner;
+
+public class ques6 {
+
+	 public static void main(String[] args) {
+	      String str = "anish malhotra ";  
+	        System.out.println("Original String = " + str);  
+	        int index1 = str.charAt(0);  
+	        int index2 = str.charAt(10);  
+	  
+	        System.out.println("The character at position 0 is " +  
+	            (char)index1);  
+	        System.out.println("The character at position 10 is " +  
+	            (char)index2);  
+	    }  
+	}  
