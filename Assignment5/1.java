c29b0417688e7d989f9965a2cddba4b34c2926c3
Anish Malhotra <technophile.m7@gmail.com>
2018-06-23 22:51:25 +0530

    sol 1

---
+++ Assignment5/1.java
@@ -0,0 +1,18 @@
+package as5Q1;
+
+public class ques1 {
+
+	 public static void main(String[] args) {
+         int i,d = 0;
+		 int A[]={1,2,3,4,5};
+		 for(i=0;i<5;i++)
+		 {
+			 d=d+A[i];
+		 }
+int s=d/5;
+System.out.println(s);
+
+	   
+	 }
+	}
+
