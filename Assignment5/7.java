170675b37573dcec384faf1dc90fb1a2329a169b
Anish Malhotra <technophile.m7@gmail.com>
2018-06-23 22:53:28 +0530

    sol 7

---
+++ Assignment5/7.java
@@ -0,0 +1,17 @@
+package as5Q7;
+
+public class ques7 {
+
+
+    public static void main(String[] args)  
+    {  
+        String str1 = "Anish";  
+        String str2 = " Malhotra";  
+          
+        System.out.println("String 1: " + str1);  
+        System.out.println("String 2: " + str2);   
+        String str3 = str1.concat(str2);  
+       System.out.println("The concatenated string: " + str3);  
+    }  
+}  
+
